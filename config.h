dfbd10a1554148bdf5558c8d378959c88b2422f4
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2016-05-07 11:58:58 +0000

    making build environment for multi-plartform

---
+++ config.h
@@ -0,0 +1,32 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the `confuse' library (-lconfuse). */
+#define HAVE_LIBCONFUSE 1
+
+/* Define to 1 if you have the `pthread' library (-lpthread). */
+#define HAVE_LIBPTHREAD 1
+
+/* Name of package */
+#define PACKAGE "project"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "user.localhost2000@gmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "project"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "project 0.0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "project"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.0.1"
+
+/* Version number of package */
+#define VERSION "0.0.1"
